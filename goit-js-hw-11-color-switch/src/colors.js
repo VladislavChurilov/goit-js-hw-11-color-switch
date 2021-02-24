0e2c4fcdff36292681328c1390fbc88d8da38d97
Vladislav Churilov <88vvch88@gmail.com>
2021-02-25 00:56:04 +0200

    hw11

--- goit-js-hw-11-color-switch/src/colors.js
+++ goit-js-hw-11-color-switch/src/colors.js
@@ -6,32 +6,24 @@ const colors = [
     '#009688',
     '#795548',
   ];
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+ 
   const bodyRef = document.querySelector('body');  
   const startBtnRef = document.querySelector('.start');
   const stopBtnRef = document.querySelector('.stop');
+
+  startBtnRef.addEventListener('click', addColor);     
   
-
-  const color = colors.forEach(el => 
-    console.log(el)); 
-//   function addColor() {
-//       console.log('ooooo');
-//      const inegrId = setInterval(color,1000, color =>
-//     body.style.background = color.el
-//     );  
-//   }  
-  const addColor = () => {
-    setInterval(() => {
-        console.log('uuuuuu');
-
-    }, 1000);
-  };
-  startBtnRef.addEventListener('click', addColor);
-  function ddd(params) {
-      
+  function addColor() {
+const integrId = setInterval(function() {
+  let currentIndex = randomIntegerFromInterval(0,5); 
+    bodyRef.style.background = colors[currentIndex];    
+    stopBtnRef.addEventListener('click',event => {  
+    clearInterval(integrId);    
+  });    
+}, 1000);
   }
-  const color = colors.forEach(el => {
-    console.log(color.el);
-bodyRef.style.background = color.el;
+
+  function randomIntegerFromInterval  (min, max)  {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+// можно ли в качестве ID использовать this, а не делать переменную из setInterval? C this тоже работает.
