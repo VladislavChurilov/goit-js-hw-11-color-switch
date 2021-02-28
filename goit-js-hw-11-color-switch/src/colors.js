62c8212c6c5dab8293c39d87a074531dd3f089d1
Vladislav Churilov <88vvch88@gmail.com>
2021-02-28 14:21:33 +0200

    hw11 corrected

--- goit-js-hw-11-color-switch/src/colors.js
+++ goit-js-hw-11-color-switch/src/colors.js
@@ -11,19 +11,19 @@ const colors = [
   const startBtnRef = document.querySelector('.start');
   const stopBtnRef = document.querySelector('.stop');
 
-  startBtnRef.addEventListener('click', addColor);     
-  
+  startBtnRef.addEventListener('click', addColor);  
+
   function addColor() {
 const integrId = setInterval(function() {
   let currentIndex = randomIntegerFromInterval(0,5); 
-    bodyRef.style.background = colors[currentIndex];    
-    stopBtnRef.addEventListener('click',event => {  
-    clearInterval(integrId);    
-  });    
+    bodyRef.style.background = colors[currentIndex];     
+  
 }, 1000);
-  }
+  stopBtnRef.addEventListener('click',event => {  
+    clearInterval(integrId);      
+  });
+}  
 
   function randomIntegerFromInterval  (min, max)  {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
-// можно ли в качестве ID использовать this, а не делать переменную из setInterval? C this тоже работает.
